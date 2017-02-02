ebcbd9cdf5e6bd5680ee01f844e3410b154743de
Jon Manock <jonmanock@gmail.com>
2017-02-02 12:04:44 -0500

    good things happening

---
+++ src/app/store/breeds.js
@@ -0,0 +1,5 @@
+angular
+  .module('app')
+  .component('fountainBreeds', {
+    templateUrl: 'app/store/breeds.html'
+  });
---
+++ src/app/store/states.js
@@ -0,0 +1,5 @@
+angular
+  .module('app')
+  .component('fountainState', {
+    templateUrl: 'app/store/states.html'
+  });
