f627eb39e2bcd8db4d04c6fcc9e84fed5a4e0e42
Jon Manock <jonmanock@gmail.com>
2017-03-19 10:36:38 -0400

    update for build

---
+++ src/app/main/main.controller.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('quotes2017')
+    .controller('MainController', MainController);
+
+  /** @ngInject */
+  function MainController($timeout, webDevTec, toastr) {
+    var vm = this;
+
+
+    console.log('HelloFriend');
+  }
+})();
